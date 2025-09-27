515ba48148480336e6ec52751e2750f49cb16eb6
NehalHamada <nehalhamada3@gmail.com>
2025-09-27 16:16:55 +0300

    first committ

--- src/App.jsx
+++ src/App.jsx
@@ -235,7 +235,7 @@ function App() {
                 <CardMedia
                   component="img"
                   height="220"
-                  image="../public/shrimp.jpg"
+                  image="https://www.pinterest.com/pin/774124930908687/"
                   alt="Paella dish"
                 />
                 <CardContent>
